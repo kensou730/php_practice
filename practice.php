4b5e29bad68494ae098441193f2be907950b9b19
20_Ishikawa_Kensho <lydin_note@yahoo.co.jp>
2019-07-18 11:34:24 +0000

    tech boost Task 2019/07/18

--- practice.php
+++ practice.php
@@ -52,4 +52,23 @@ echo $2018_calendar[December];
 
 */
 
+// --------------------------------
+// 修正後
+// --------------------------------
+$calendar_2018 = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+    
+    ];
+    echo $calendar_2018['December'];
 ?>
