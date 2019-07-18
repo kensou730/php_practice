b54082d7e5147deb965520e17507dc4a0f7b2789
20_Ishikawa_Kensho <lydin_note@yahoo.co.jp>
2019-07-18 03:46:44 +0000

    tech boost Task

--- practice.php
+++ practice.php
@@ -1,22 +1,55 @@
 <?php
-$value = 10;
-$result = ($value < 20) ? '$valueは２０より小さいです' : '$valueは２０と同様、または大きいです。';
-
-echo $result;
-
+// ----------------------------
+// Task1
+// ----------------------------
+$a = 3;
+$b = 7;
+echo $a+$b;
+// ----------------------------
+// Task2
+// ----------------------------
 for($i=1;$i<=12;$i++){
     $array[] = $i;
     // echo $array[$i];
 }
-echo "今月は{$array[7]}月です。";
+echo "来月は{$array[8]}月です。";
+// ----------------------------
+// Task3
+// ----------------------------
+$hello  =   "Hello, ";
+$name   =   "Kensho";
+$world  =   "'s World!";
 
-$drink = [
-    "coffee" => 100,
-    "juice" => 120,
-    "Beer" => 200
-    ];
-    foreach($drink as $drink => $val){
-        echo "{$drink}は{$val}円です。";
-    }
-    var_dump($drink);
+echo $hello.$name.$world;
+
+// ----------------------------
+// Task4
+// ----------------------------
+$tech_boost =   "tech ";
+$tech_boost .= "boost";
+echo $tech_boost;
+// ----------------------------
+// Task5
+// ----------------------------
+/*
+$2018_calendar = {
+  "January" => "1月",
+  "February" => "2月"、
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" = "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => 10月,
+  "November" => "11月",
+  "December" => "12月"
+};
+
+// 12月を表示する
+echo $2018_calendar[December];
+
+*/
+
 ?>
